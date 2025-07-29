42e94144053ad59c89c46085efb6974940debca4
catharv-45 <158123475+catharv-45@users.noreply.github.com>
2025-07-29 02:21:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strictt" // treat all js code as newer version
+
+alert(3+3);  // cannnot open using nodejs we need browser 
+console.log(3+3);
+console.log("atharv");
+
+// number => 2 to power  53
+ // bigint 
+ // string => ""
+ //boolean => true/false
+ // null => standalone value
+ // undefined =>
+// symbol =>unique 
+ 
+
+ //object 
+ console.log(typeof "atharv")
+ console.log(typeof 3)
+ console.log(typeof undefined) // undefined 
+
+console.log(typeof null) // it show object
