275ca4e1c2720350965022482aef890c3b665c6a
catharv-45 <158123475+catharv-45@users.noreply.github.com>
2025-08-02 11:53:41 +0000

    Understanding while loop

---
+++ 05_interation/while.js
@@ -0,0 +1,30 @@
+// While loop 
+
+let i = 0;
+while (i<=10){
+   console.log(`Value of index is ${i}`);
+   i++;
+}
+let myArray = ["flash", "superman","batman"];
+
+let arr = 0;
+while(arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+
+// do-while loop
+
+/* let score = 1 ;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);*/
+
+let score = 11 ;  // it run must one time only difference between while loop
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
