2a789f26fd3a5ecd5ecde7623d711d1931d3797a
catharv-45 <158123475+catharv-45@users.noreply.github.com>
2025-08-03 02:20:02 +0000

    for each loop concept

---
+++ 05_interation/for_each.js
@@ -0,0 +1,41 @@
+const coding = ["js", "ruby", "java" , "python" , "cpp"];
+
+coding.forEach(function name(item) {
+    // console.log(item);
+} )
+
+coding.forEach((item) => {
+    // console.log(item)
+})
+
+function printMe(item){
+   // console.log(item);
+
+}
+coding.forEach(printMe)
+
+coding.forEach( (item , index, arr) => {
+      console.log(item,index,arr); // It show array item value index of that value and all array
+})
+
+const myCoding  = [
+    {
+        languageName : "JavaScript",
+        languageFileName: "JavaScript"
+
+    },
+    {
+        languageName : "python",
+        languageFileName: "puthon"
+
+    },
+    {
+        languageName : "java",
+        languageFileName: "java"
+
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName); // accessing the language name value
+})
