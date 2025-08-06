98ba125a4e9e1e38817d6621704fd50d4a7c420a
catharv-45 <158123475+catharv-45@users.noreply.github.com>
2025-08-06 02:23:08 +0000

    for each loop concept

--- 05_interation/for_each.js
+++ 05_interation/for_each.js
@@ -5,11 +5,11 @@ coding.forEach(function name(item) {
 } )
 
 coding.forEach((item) => {
-    // console.log(item)
+    console.log(item)
 })
 
 function printMe(item){
-   // console.log(item);
+    // console.log(item);
 
 }
 coding.forEach(printMe)
