58e1f69e9946122d55a758ffd4dac8a1073c5dd4
catharv-45 <158123475+catharv-45@users.noreply.github.com>
2025-08-15 02:36:45 +0000

    Method for show data and time

--- 01_basics/07_Datetime.js
+++ 01_basics/07_Datetime.js
@@ -1,18 +1,18 @@
 // Dates
 
 let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toLocaleString());
+console.log(myDate.toString()); // Fri Aug 15 2025 02:16:54 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toLocaleString()); // 8/15/2025, 2:16:54 AM
 
 console.log(typeof myDate);    // Object
 
 //let myCreatedDate = new Date(2023,0,23)
 //let myCreatedDate = new Date(2023 ,0, 23 , 5 , 3)// Mon Jan 23 2023  5:03:00
 let myCreatedDate = new Date("2025-07-30")
-console.log(myCreatedDate.toDateString());  // Mon Jan 23 2023
+console.log(myCreatedDate.toDateString());  // Wed Jul 30 2025
 
 let mylocalDate = new Date("01-14-2025")
-console.log(mylocalDate.toLocaleString());  // As indian origin
+console.log(mylocalDate.toLocaleString());  // 1/14/2025, 12:00:00 AM
 
 let myTimeStamp = Date.now()
 
