6ae353da5467ccb70a20bf1faddae4d3083fb0a5
catharv-45 <158123475+catharv-45@users.noreply.github.com>
2025-08-19 12:43:02 +0000

    asynchronous javascript

---
+++ 08_Event/Async.js
@@ -0,0 +1,13 @@
+//watch full video no 37
+
+// default javascript:
+// javascript is synchronous means after compliction of one code next code will be executed
+//javascript is single threaded 
+
+//execution context:
+// execute one line of code at a time
+// each operation waits for the last one to complete before executing
+
+// there are two types of javascript code are write 
+// 1. blocking code (block the flow of Program -> read file sync)
+// 2. non blocking code (Does not block execution -> read file Async)
