02f3130eacb89aeb333d68929060644c5ab04e9f
catharv-45 <158123475+catharv-45@users.noreply.github.com>
2025-08-22 16:34:57 +0000

    promise concept

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -55,3 +55,48 @@ promiseFour.then((user)=>{ // chainning
 }).catch(function(error){
     console.log(error); // also check for false 
 }).finally(()=> console.log("The promise is either resolved or rejected"))
+
+const promisefive = new Promise((resolve,reject)=>{
+setTimeout(function(){
+        let error = true; 
+        if(!error){
+            resolve({username:"javascript",password:123})
+        }else{
+            reject('Error: JS went wrong')
+        }
+    },1000)
+});
+
+async function consumePromiseFive(){
+   try{
+       const response = await promisefive;
+       console.log(response);
+   }catch(error){
+        console.log(error);
+   }
+
+}
+consumePromiseFive()
+
+/*async function getAllUsers(){
+    try {
+       const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data);
+      } 
+     catch (error) {
+        console.log("Error: ",error);
+
+    }
+}*/
+
+
+//getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+}).then((data)=>{
+    console.log(data);
+})
+.catch((error)=> console.log(error))
