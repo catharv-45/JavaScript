2e9f80696e77462e4412871cfd8f0676e971bcfe
Atharv Ashvin Chavan <158123475+catharv-45@users.noreply.github.com>
2025-09-03 08:19:34 +0000

    pi problem

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,29 @@
+
+// console.log(Math.PI);  // 3.14159265
+// Math.PI =5
+// console.log(Math.PI); // it does not override output: 3.14159265
+
+
+const descripter =  Object.getOwnPropertyDescriptor(Math,'PI')
+console.log(descripter);
+
+const chai = {
+    name:'patil',
+    price:10,
+    isAvailable :true
+}
+//console.log(chai);
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+// define specific property in object like
+//{
+  //value: 'patil',
+  //writable: true,
+  //enumerable: true,
+  //configurable: true
+//}
+
+Object.defineProperties(chai,'name',{
+    writable:false,
+    enumerable:false
+})
