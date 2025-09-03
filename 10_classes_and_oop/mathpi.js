e729ddaa21d6171d2755efe013756ba3f37536e8
Atharv Ashvin Chavan <158123475+catharv-45@users.noreply.github.com>
2025-09-03 14:01:04 +0000

    use of getOwnPropertyDescriptor  property

--- 10_classes_and_oop/mathpi.js
+++ 10_classes_and_oop/mathpi.js
@@ -10,7 +10,11 @@ console.log(descripter);
 const chai = {
     name:'patil',
     price:10,
-    isAvailable :true
+    isAvailable :true,
+
+    orderChai:function(){
+      console.log('hello');
+    }
 }
 //console.log(chai);
 console.log(Object.getOwnPropertyDescriptor(chai,"name"));
@@ -23,7 +27,16 @@ console.log(Object.getOwnPropertyDescriptor(chai,"name"));
   //configurable: true
 //}
 
-Object.defineProperties(chai,'name',{
+/*Object.defineProperties(chai,'name',{
     writable:false,
     enumerable:false
-})
+})*/
+
+console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+
+for (let [key,value] of Object.entries(chai)) {
+  if(typeof value !== 'function'){
+    console.log(`${key}: ${value}`);
+  }
+  
+}
